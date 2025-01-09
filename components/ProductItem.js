f78284494a581ba402c06b83f5565cb7c4efe89d
mito001 <85709457+mito001@users.noreply.github.com>
2025-01-09 01:31:19 -0300

    feat: Show product description on hover using Tooltip

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,29 +1,29 @@
-import Link from 'next/link'
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
+import React from 'react';
+import Link from 'next/link';
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Tooltip } from '@mui/material';
 
 export default function ProductItem({ product }) {
   return (
-    <Card className="mb-4">
-      <Link href={`/product/${product.slug}`} passHref>
-        <CardActionArea>
-          <CardMedia 
-            component="img"
-            image={product.image}
-            title={product.name}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {product.name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p" className="mb-2">
-              {product.description?.slice(0, 80)}...
-            </Typography>
-            <Typography variant="h6" color="primary">
-              ${product.price}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Link>
-    </Card>
-  )
+    <Tooltip title={product.description} placement="top" arrow>
+      <Card className="mb-4">
+        <Link href={`/product/${product.slug}`} passHref>
+          <CardActionArea>
+            <CardMedia 
+              component="img"
+              image={product.image}
+              title={product.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.name}
+              </Typography>
+              <Typography variant="h6" color="primary">
+                ${product.price}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Link>
+      </Card>
+    </Tooltip>
+  );
 }
