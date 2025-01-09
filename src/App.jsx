733008f60bb642c283f07a71ed5e3624f7de8a1e
mito001 <85709457+mito001@users.noreply.github.com>
2025-01-09 18:56:55 -0300

    Add basic navigation system

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import KitchenProducts from './components/KitchenProducts'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home')
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'home':
+        return <Home />
+      case 'products':
+        return <KitchenProducts />
+      case 'about':
+        return <About />
+      case 'contact':
+        return <Contact />
+      default:
+        return <Home />
+    }
+  }
+
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
-      <KitchenProducts />
+      <Navbar setCurrentPage={setCurrentPage} />
+      {renderPage()}
     </div>
   )
 }
